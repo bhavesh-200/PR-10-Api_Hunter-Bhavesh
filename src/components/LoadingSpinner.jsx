e0812dd3fa5cb81a8627aff9b066e6f8cf8a6ad3
bhavesh-200 <bhaveshvaryani2@gmail.com>
2025-03-01 09:58:08 +0530

    first commit

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoadingSpinner() {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+      <p className='p'>Loading...</p>
+    </div>
+  )
+}
+
+export default LoadingSpinner;
